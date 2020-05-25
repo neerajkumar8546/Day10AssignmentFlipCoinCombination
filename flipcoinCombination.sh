69385e61faa975f40201f001e6c54b99883f1c3c
unknown <neerajkumar8546@gmail.com>
2020-05-25 20:36:38 +0530

    doublet Combination store dictionary and find percentage

--- flipcoinCombination.sh
+++ flipcoinCombination.sh
@@ -40,3 +40,67 @@ perHead=`echo "scale=2;((${resultHead[@]} / $iteration))" | bc -l`
 echo "Percentage Of Head : " `echo "scale=2;(($perHead* 100))" | bc -l`
 perTail=`echo "scale=2;((${resultTail[@]} / $iteration))" | bc -l`
 echo "Percentage Of Tail : " `echo "scale=2;(($perTail* 100))" | bc -l`
+
+#UC3 
+
+
+--#!/bin/bash
+
+#float value only cal in mobaxterm not cal git bash
+
+declare -A resultHH
+declare -A resultHT
+declare -A resultTH
+declare -A resultTT
+
+read -p "How many time do you want to toss" n
+
+
+for (( i=0;i<$n;i++))
+do
+
+   flip1=$((RANDOM%2))
+   flip2=$((RANDOM%2))
+
+   if [[ $flip1 -eq 1 ]]
+   then
+
+      if [[ $flip2 -eq 1 ]]
+      then
+         resultHH["HH"]=$((${resultHH["HH"]}+1))
+      else
+            resultHT["HT"]=$((${resultHT["HT"]}+1))
+      fi
+
+   else
+
+      if [[ $flip2 -eq 1 ]]
+      then
+            resultTH["TH"]=$((${resultTH["TH"]}+1))
+      else
+            resultTT["TT"]=$((${resultTT["TT"]}+1))
+
+      fi
+
+   fi
+
+
+done
+
+echo "${resultHH[@]}"
+echo "${resultHT[@]}"
+echo "${resultTT[@]}"
+echo "${resultTH[@]}"
+
+
+echo The win percentage of each combination in the doublet coin toss:
+
+perHH=`echo "scale=2;((${resultHH[@]} / $n))" | bc -l`
+echo "Percentage Of HH  : " `echo "scale=2;(($perHH * 100))" | bc -l` "%"
+perHT=`echo "scale=2;((${resultHT[@]} / $n))" | bc -l`
+echo "Percentage Of HT : " `echo "scale=2;(($perHT* 100))" | bc -l` "%"
+perTT=`echo "scale=2;((${resultTT[@]} / $n))" | bc -l`
+echo "Percentage Of TT : " `echo "scale=2;(($perTT* 100))" | bc -l` "%"
+perTH=`echo "scale=2;((${resultTH[@]} / $n))" | bc -l`
+echo "Percentage Of TH : " `echo "scale=2;(($perTH* 100))" | bc -l` "%"
+
